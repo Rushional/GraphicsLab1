5ab1d41753976c75e690be35a0e4de85009e000d
Rushional <rushional2@gmail.com>
2020-03-09 19:30:07 +0300

    Made model more readable

--- src/model/Lab1Model.java
+++ src/model/Lab1Model.java
@@ -4,7 +4,10 @@ import org.eclipse.swt.graphics.Point;
 import Jama.Matrix;
 
 public class Lab1Model {
-	private static Matrix defaultTriangle = new Matrix(new double[][] {{750.0, 250.0, 1.0},{850.0, 250.0, 1.0},{850.0, 150.0, 1.0}});
+	private static Matrix defaultTriangle = new Matrix(new double[][] {
+			{750.0, 250.0, 1.0},
+			{850.0, 250.0, 1.0},
+			{850.0, 150.0, 1.0}});
 	private Matrix currentTriangle;
 	
 	public Matrix getDefaultTriangle(){
@@ -12,10 +15,19 @@ public class Lab1Model {
 	}
 	
 	public void changeTriangle(Point point, double angle){
-		Matrix changeSK = new Matrix(new double[][] {{1.0, 0.0, 0.0},{0.0, 1.0, 0.0},{-point.x, -point.y, 1.0}});
+		Matrix changeSK = new Matrix(new double[][] {
+				{1.0, 0.0, 0.0},
+				{0.0, 1.0, 0.0},
+				{-point.x, -point.y, 1.0}});
 		angle = Math.toRadians(angle);
-		Matrix rotate = new Matrix(new double[][] {{Math.cos(-angle), Math.sin(-angle), 0.0},{-Math.sin(-angle), Math.cos(-angle), 0.0},{0.0, 0.0, 1.0}});
-		Matrix changeSKback = new Matrix(new double[][] {{1.0, 0.0, 0.0},{0.0, 1.0, 0.0},{point.x, point.y, 1.0}});
+		Matrix rotate = new Matrix(new double[][] {
+				{Math.cos(-angle), Math.sin(-angle), 0.0},
+				{-Math.sin(-angle), Math.cos(-angle), 0.0},
+				{0.0, 0.0, 1.0}});
+		Matrix changeSKback = new Matrix(new double[][] {
+				{1.0, 0.0, 0.0},
+				{0.0, 1.0, 0.0},
+				{point.x, point.y, 1.0}});
 		Matrix changedTriangle = defaultTriangle.times(changeSK); //times - умножение матриц
 		changedTriangle = changedTriangle.times(rotate);
 		changedTriangle = changedTriangle.times(changeSKback);
